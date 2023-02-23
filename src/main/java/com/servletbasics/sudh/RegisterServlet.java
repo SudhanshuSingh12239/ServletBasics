72c93b19663e794e8487216ce49f08558562490e
Sudhanshu Singh <sudhanshusingh12239@gmail.com>
2023-02-23 16:52:56 +0530

    Form Registration Using Get Method.

--- src/main/java/com/servletbasics/sudh/RegisterServlet.java
+++ src/main/java/com/servletbasics/sudh/RegisterServlet.java
@@ -16,6 +16,35 @@ public class RegisterServlet extends HttpServlet {
         message = "Hello World!";
     }
 
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException ,IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("Executing Get Method.......");
+        out.println("<h2>Welcome! To Register Servlet</h2?");
+        String name = request.getParameter("user_name");
+        String password = request.getParameter("user_password");
+        String email = request.getParameter("user_email");
+        String gender = request.getParameter("user_gender");
+        String course = request.getParameter("user_course");
+
+        String cond = request.getParameter("condition");
+        if(cond != null){
+            out.println("<h2>Name : " + name + "</h2>");
+            out.println("<h2>Password : " + password + "</h2>");
+            out.println("<h2>Email : " + email + "</h2>");
+            out.println("<h2>Gender : " + gender + "</h2>");
+            out.println("<h2>Course : " + course + "</h2>");
+            printOnConsole(name,password,email,gender,course);
+
+        }
+        else{
+            out.println("<h1>You have not checked the Terms & Conditions !!!</h1>");
+            System.out.println("You have not checked the Terms & Conditions !!!");
+        }
+
+    }
+
+
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException ,IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
