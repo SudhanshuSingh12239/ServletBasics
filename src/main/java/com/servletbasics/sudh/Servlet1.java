ace18d199e0d80ffb127e3a45c1c6c1fb2c6d4e2
Sudhanshu Singh <sudhanshusingh12239@gmail.com>
2023-03-01 22:09:51 +0530

    Problem of Stateless in Servlet .

---
+++ src/main/java/com/servletbasics/sudh/Servlet1.java
@@ -0,0 +1,28 @@
+package com.servletbasics.sudh;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Servlet1 extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title> Servlet Servlet2 </title>");
+        out.println("</head");
+        out.println("<body>");
+        String name = req.getParameter("name");
+        out.println("<h1> Hello, " + name + " ! This is My Website Using First Servlet......</h1>");
+        out.println("<h1><a href='servlet2'>Go To Servlet2</a></h1> ");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
